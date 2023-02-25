03cf077f2fa8f1227a0e04c06270dad6023a57e6
m4idy <maidy2234507@correo.uis.edu.co>
2023-02-25 08:27:25 -0500

    operadores_aritmetrico

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # operadores_aritmetico
 programar y implementar operadores aritméticos en python 
+
+## Diseño 
+
+### Diagrama de flujo 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
---
+++ operadores_aritmetico.py
@@ -0,0 +1,36 @@
+# programa para implementar operadores aritmeticos
+
+print("........................................")
+print("..........OPERADORES ARITMERICOS........")
+print("........................................")
+
+# imput 
+
+x= int(input("digite el valor de x"))
+y= int(input("digite el valor de y"))
+
+# procesing
+
+s=x+y
+r=x-y
+m=x*y
+d=x/y
+mod=x%y
+de= x//y
+p=x**y
+
+
+#output
+
+print(".........................................") 
+print(".............RESULTADO...................")
+print(".........................................")
+print("suma: " + str(s))
+print("resta: " + str(r))
+print("multiplicacion: " + str(m))
+print("divicion: " + str(d))
+print("modulo: " + str(mod))
+print("divison entera: " + str(de))
+print("potencia: " + str(p))
+
+  
